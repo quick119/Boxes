12c8a5a6098374aa8c5b85cc3da15d48afd57369
quick <quick.wang@gmail.com>
2018-12-06 21:07:20 +0800

    done

--- app/src/main/java/post/com/boxes/ResultActivity.java
+++ app/src/main/java/post/com/boxes/ResultActivity.java
@@ -10,6 +10,21 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        TextView boxName = findViewById(R.id)
+        TextView boxName = findViewById(R.id.name);
+        TextView dimention = findViewById(R.id.dimention);
+        TextView price = findViewById(R.id.price);
+        int type = getIntent().getIntExtra("BOX", Box.TYPE_3);
+        Box box = null;
+        switch (type) {
+            case Box.TYPE_3:
+                box = new Box3();
+                break;
+            case Box.TYPE_5:
+                box = new Box5();
+                break;
+        }
+        boxName.setText(box.name);
+        dimention.setText(box.length + "/" + box.width + "/" + box.height);
+        price.setText(box.price + "");
     }
 }
